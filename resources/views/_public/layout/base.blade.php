d293eac84f9698a9eeb809e4cc613bc04b3f9963
Igor Russkikh <igor@a2design.biz>
2022-11-27 17:05:42 +0600

    CO-1: Add assets. Compile with Vite

--- resources/views/_public/layout/base.blade.php
+++ resources/views/_public/layout/base.blade.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        @vite(['resources/scss/main.scss', 'resources/js/app.js'])
+    </head>
     <body class="">
         <div class="page-wrap">
             @include('_public.parts.header')
